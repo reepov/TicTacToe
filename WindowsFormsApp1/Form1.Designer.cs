c3d097f59fdca7683bc90eb1082639f496f90f25
reepov <mix.reep@mail.ru>
2022-02-28 00:52:06 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1023, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Счёт:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1065, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1013, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Крестики";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1089, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Нолики";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1013, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Первыми ходят крестики";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1013, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Начать заново";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(982, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(77, 46);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(1092, 63);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(80, 46);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1013, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 56);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сбросить счёт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 961);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int         winKrestik              = 0;              // === Счёт крестиков ======================== // 
+        int         winNolik                = 0;              // === Счёт ноликов ========================== //
+        byte        check                   = 0;              // === Проверка ничьи ======================== //
+        string      krestikWin;                               // === Строка для вывода счёта крестиков ===== //
+        string      nolikWin;                                 // === Строка для вывода счёта ноликов ======= //
+        bool        playDenied              = false;          // === Запрет на нажатие после победы ======== //
+        int[,]      arrayField              = new int[3, 3];  // === Состояние поля ======================== //
+        bool[,]     arrayLogicFieldKrestiki = new bool[3, 3]; // === Проверка нажатия на клетку крестика === //
+        bool[,]     arrayLogicFieldNoliki   = new bool[3, 3]; // === Проверка нажатия на клетку нолика ===== //
+        public bool krestikiPlayer          = true;           // === Ход крестика ========================== //
+        public bool nolikiPlayer            = false;          // === Ход нолика ============================ //
+        public bool krestikiWin             = false;          // === Победа крестика ======================= //
+        public bool nolikiWin               = false;          // === Победа нолика ========================= //
+        public bool draw                    = false;
+
+        /// <summary>
+        /// Параметры запуска формы
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Width                      = 1200;  // === Ширина формы ============================== //
+            this.Height                     = 1000;  // === Высота формы ============================== //
+            MaximizeBox                     = false; // === Запрет на раширение формы во весь экран === //
+
+            this.MouseClick                += new MouseEventHandler(Form1_MouseClick);
+
+            for (int i = 0; i < 3; i++)              // === Инициализация логических матриц =========== //
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arrayLogicFieldKrestiki[i, j] = false;
+                    arrayLogicFieldNoliki[i, j]   = false;
+                }
+            }
+        }
+        /// <summary>
+        /// Параметры, устанавливаемые при запуске формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "0";
+            richTextBox2.Text = "0";
+        }
+        /// <summary>
+        /// Обработчик событий нажатия мыши
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (playDenied == true) return; // === Не реагировать на мышь, если победа === //
+
+            if (e.X >= 0 && e.X < 300)      // === Клетки по координатам === //
+            {
+                if (e.Y >= 0 && e.Y <= 300 && arrayField[0, 0] == 0 )
+                    arrayField[0, 0] = 1;
+                if (e.Y > 300 && e.Y <= 600 && arrayField[0, 1] == 0)
+                    arrayField[0, 1] = 1;
+                if (e.Y > 600 && e.Y <= 900 && arrayField[0, 2] == 0)
+                    arrayField[0, 2] = 1;
+            }
+
+            else if (e.X >= 300 && e.X < 600)
+            {
+                if (e.Y >= 0 && e.Y <= 300 && arrayField[1, 0] == 0)
+                    arrayField[1, 0] = 1;
+                if (e.Y > 300 && e.Y <= 600 && arrayField[1, 1] == 0)
+                    arrayField[1, 1] = 1;
+                if (e.Y > 600 && e.Y <= 900 && arrayField[1, 2] == 0)
+                    arrayField[1, 2] = 1;
+            }
+
+            else if (e.X >= 600 && e.X < 900)
+            {
+                if (e.Y >= 0 && e.Y <= 300 && arrayField[2, 0] == 0)
+                    arrayField[2, 0] = 1;
+                if (e.Y > 300 && e.Y <= 600 && arrayField[2, 1] == 0)
+                    arrayField[2, 1] = 1;
+                if (e.Y > 600 && e.Y <= 900 && arrayField[2, 2] == 0)
+                    arrayField[2, 2] = 1;
+            }
+
+            else return;
+
+            Invalidate();  
+        }
+        /// <summary>
+        /// Рисование фигур и поля
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+            Graphics graphics = e.Graphics;      // ==== подключение элемента graphics ==== //
+
+            graphics.TranslateTransform(5, 5);   // ==== перенесение центра =============== //
+
+            for (int i = 0; i < 4; i++)          // ==== вычерчивание поля ================ //
+            {
+                graphics.DrawLine(new Pen(Color.Black, 3.0f), 300 * i, 0, 300 * i, 900);
+                graphics.DrawLine(new Pen(Color.Black, 3.0f), 0, 300 * i, 900, 300 * i);
+            }
+            Redraw();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {         
+                    if (arrayField[i,j] == 1) // === Если была нажата кнопка === //
+                    {
+                        if (krestikiPlayer)
+                        {
+                            graphics.DrawLine(new Pen(Color.Red, 20.0f), 50 + 300 * i, 50 + 300 * j, 300 * (i + 1) - 50, 300 * (j + 1) - 50);
+                            graphics.DrawLine(new Pen(Color.Red, 20.0f), 300 * (i + 1) - 50, 300 * j + 50, 50 + 300 * i, 300 * (j + 1) - 50);
+                            Count(arrayLogicFieldKrestiki, i, j);
+                        }
+                        else if (nolikiPlayer)
+                        {
+                            graphics.DrawEllipse(new Pen(Color.Blue, 20.0f), 50 + 300 * i, 50 + 300 * j, 200, 200);
+                            Count(arrayLogicFieldNoliki, i, j);
+                        }
+                    }
+                }
+            }
+            void Count(bool [,] arrayLogic, int i, int j) // === Каждый ход проверка на победу === //
+            {
+                Redraw();
+                arrayLogic[i, j] = true;
+                arrayField[i, j] = 2;
+                CheckWinner();
+                krestikiPlayer = !krestikiPlayer;
+                nolikiPlayer = !nolikiPlayer;
+            }
+            void Redraw() // === Отрисовка пропадающих фигур === //
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (arrayLogicFieldKrestiki[i, j] == true)
+                        {
+                            graphics.DrawLine(new Pen(Color.Red, 20.0f), 50 + 300 * i, 50 + 300 * j, 300 * (i + 1) - 50, 300 * (j + 1) - 50);
+                            graphics.DrawLine(new Pen(Color.Red, 20.0f), 300 * (i + 1) - 50, 300 * j + 50, 50 + 300 * i, 300 * (j + 1) - 50);
+                        }
+                        if (arrayLogicFieldNoliki[i, j] == true)
+                        {
+                            graphics.DrawEllipse(new Pen(Color.Blue, 20.0f), 50 + 300 * i, 50 + 300 * j, 200, 200);
+                        }
+                    }
+                }
+            }
+            void CheckWinner() // === Победные случаи === //
+            {    
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 1, 1, 3, 1, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 1, 2, 3, 2, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 1, 3, 3, 3, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 1, 1, 1, 3, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 2, 1, 2, 3, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 3, 1, 3, 3, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 1, 1, 3, 3, out winKrestik); 
+                CheckWin(arrayLogicFieldKrestiki, winKrestik, krestikWin, 3, 1, 1, 3, out winKrestik); 
+
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 1, 1, 3, 1, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 1, 2, 3, 2, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 1, 3, 3, 3, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 1, 1, 1, 3, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 2, 1, 2, 3, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 3, 1, 3, 3, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 1, 1, 3, 3, out winNolik); 
+                CheckWin(arrayLogicFieldNoliki, winNolik, nolikWin, 3, 1, 1, 3, out winNolik);
+                if (draw & !krestikiWin & !nolikiWin)
+                {
+                    playDenied = true;
+                    draw = true;
+                    Redraw();
+                    MessageBox.Show("Ничья!");
+                    check = 0;
+                    return;
+                };
+            }
+            int CheckWin(bool[,] arrayLogic, int win, string winCount, int ic, int jc, int ik, int jk, out int won) // === Проверка на победу === //
+            {
+                won = win;
+                if (jc == jk)
+                {
+                    if (arrayLogic[ic - 1, jc - 1] & arrayLogic[ic, jc - 1] & arrayLogic[ic + 1, jc - 1])
+                    {
+                        playDenied = true;
+                        win++;
+                        won = win;
+                        winCount = Convert.ToString(win);
+                        Redraw();
+                        graphics.DrawLine(new Pen(Color.Yellow, 10.0f), 0, 150 * (2 * jk - 1), 900, 150 * (2 * jk - 1)); 
+                        if (krestikiPlayer) 
+                        { 
+                            richTextBox1.Text = winCount;
+                            krestikiWin = true;
+                            MessageBox.Show("Выиграли крестики");
+                            return won;
+                        }
+                        if (nolikiPlayer) 
+                        { 
+                            richTextBox2.Text = winCount;
+                            nolikiWin = true;
+                            MessageBox.Show("Выиграли нолики");
+                            return won;
+                        }
+                        return won;
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (arrayField[i, j] == 2)
+                            {
+                                check++;
+                            }
+                        }
+                    }
+                    if (check == 9 & !krestikiWin & !nolikiWin)
+                    {
+                        draw = true;
+                    }
+                    check = 0;
+                    return won;
+                }
+                if (ic == ik)
+                {
+                    if (arrayLogic[ic - 1, jc - 1] & arrayLogic[ic - 1, jc] & arrayLogic[ic - 1, jc + 1])
+                    {
+                        playDenied = true;
+                        win++;
+                        winCount = Convert.ToString(win);
+                        Redraw();
+                        graphics.DrawLine(new Pen(Color.Yellow, 10.0f), 150 * (2 * ik - 1), 0, 150 * (2 * ik - 1), 900);
+                        if (krestikiPlayer)
+                        {
+                            krestikiWin = true;
+                            richTextBox1.Text = winCount;
+                            MessageBox.Show("Выиграли крестики");
+                            return won;
+                          }
+                        if (nolikiPlayer)
+                        {
+                            richTextBox2.Text = winCount;
+                            nolikiWin = true;
+                            MessageBox.Show("Выиграли нолики");
+                            return won;
+                        }
+                        won = win;
+                        return won;
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (arrayField[i, j] == 2)
+                            {
+                                check++;
+                            }
+                        }
+                    }
+                    if (check == 9 & !krestikiWin & !nolikiWin)
+                    {
+                        draw = true;
+                    }
+                    check = 0;
+                    return won;
+                }
+                if (ic == jc & ik == jk)
+                {
+                    if (arrayLogic[ic - 1, jc - 1] & arrayLogic[ic, jc] & arrayLogic[ic + 1, jc + 1])
+                    {
+                        playDenied = true;
+                        win++;
+                        winCount = Convert.ToString(win);
+                        Redraw();
+                        graphics.DrawLine(new Pen(Color.Yellow, 10.0f), 0, 0, 900, 900);
+                        if (krestikiPlayer)
+                        {
+                            richTextBox1.Text = winCount;
+                            krestikiWin = true;
+                            MessageBox.Show("Выиграли крестики");
+                            return won;
+                        }
+                        if (nolikiPlayer)
+                        {
+                            richTextBox2.Text = winCount;
+                            krestikiWin = true;
+                            MessageBox.Show("Выиграли нолики");
+                            return won;
+                        }
+                        won = win;
+                        return won;
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (arrayField[i, j] == 2)
+                            {
+                                check++;
+                            }
+                        }
+                    }
+                    if (check == 9 & !krestikiWin & !nolikiWin)
+                    {
+                        draw = true;
+                    }
+                    check = 0;
+                    return won;
+                }
+                if (ic == jk & ik == jc)
+                {
+                    if (arrayLogic[ic - 1, jc - 1] & arrayLogic[ic - 2, jc] & arrayLogic[ic - 3, jc + 1])
+                    {
+                        playDenied = true;
+                        win++;
+                        winCount = Convert.ToString(win);
+                        Redraw();
+                        graphics.DrawLine(new Pen(Color.Yellow, 10.0f), 900, 0, 0, 900);
+                        if (krestikiPlayer)
+                        {
+                            richTextBox1.Text = winCount;
+                            krestikiWin = true;
+                            MessageBox.Show("Выиграли крестики");
+                            return won;
+                        }
+                        if (nolikiPlayer)
+                        {
+                            richTextBox2.Text = winCount;
+                            nolikiWin = true;
+                            MessageBox.Show("Выиграли нолики");
+                            return won;
+                        }
+                        won = win;
+                        return won;
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (arrayField[i, j] == 2)
+                            {
+                                check++;
+                            }
+                        }
+                    }
+                    if (check == 9 & !krestikiWin & !nolikiWin)
+                    {
+                        draw = true;
+                    }
+                    check = 0;
+                }
+                return won;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // === Новая игра === //
+        {       
+            this.Refresh();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arrayLogicFieldKrestiki[i, j] = false;
+                    arrayLogicFieldNoliki[i, j]   = false;
+                    arrayField[i, j]              = 0;
+                }
+            }
+            krestikiPlayer = true;
+            nolikiPlayer   = false;
+            krestikiWin    = false;
+            nolikiWin      = false;
+            draw           = false;
+            playDenied     = false;
+            check          = 0;
+            Invalidate();        
+        }
+        private void button2_Click(object sender, EventArgs e) // === Сбросить счёт === //
+        {
+            richTextBox1.Text = "0";
+            richTextBox2.Text = "0";
+            winNolik          = 0;
+            winKrestik        = 0;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
